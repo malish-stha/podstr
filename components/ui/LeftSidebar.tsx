ed38de2fa67024cac3a6413819c8b711b575ddee
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-06-08 13:59:28 +0545

    "Added 'use client' directive, imported sidebarLinks and cn, and implemented sidebar links with active state"

--- components/ui/LeftSidebar.tsx
+++ components/ui/LeftSidebar.tsx
@@ -1,8 +1,14 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 const LeftSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
     <section className="left_sidebar">
       <nav className="flex flex-col gap-6">
@@ -15,6 +21,24 @@ const LeftSidebar = () => {
             Podstr
           </h1>
         </Link>
+
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
+          return (
+            <Link
+              href={route}
+              key={label}
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                { "bg-nav-focus border-r-4 border-orange-1": isActive }
+              )}
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
