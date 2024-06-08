1c169c400ce9d4b4858479536102c5390033b3ce
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-06-08 10:43:17 +0545

    "Update title and description in metadata, and modify LeftSidebar component to include logo and text"

--- components/ui/LeftSidebar.tsx
+++ components/ui/LeftSidebar.tsx
@@ -6,13 +6,14 @@ const LeftSidebar = () => {
   return (
     <section className="left_sidebar">
       <nav className="flex flex-col gap-6">
-        <Link href="/">
-          <Image
-            src="/icons/logo.svg"
-            alt="logo"
-            width={23}
-            height={27}
-          ></Image>
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+        >
+          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+            Podstr
+          </h1>
         </Link>
       </nav>
     </section>
