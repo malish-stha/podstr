4c3e1caadaa576707d92d1198e1cad0894ad6a62
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-06-08 09:37:16 +0545

    "Updated RootLayout to import and use LeftSidebar component, removed next.svg and vercel.svg files from public directory."

---
+++ components/ui/LeftSidebar.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const LeftSidebar = () => {
+  return (
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link href="/">
+          <Image
+            src="/icons/logo.svg"
+            alt="logo"
+            width={23}
+            height={27}
+          ></Image>
+        </Link>
+      </nav>
+    </section>
+  );
+};
+
+export default LeftSidebar;
