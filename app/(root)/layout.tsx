029e56fac9713e80e6d95aba7d9cc415c6dfcb44
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-06-09 07:19:56 +0545

    "Refactor layout and remove ui/LeftSidebar component"

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
-import LeftSidebar from "@/components/ui/LeftSidebar";
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,11 +8,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
         <LeftSidebar />
-        {children}
-        <p className="text-white-1">RIGHT SIDEBAR</p>
+
+        <section className="border-2 border-red-500 flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image src="/icons/logo.svg" width={30} height={30} alt="img" />
+              MobileNav
+            </div>
+            <div>
+              Toaster
+              {children}
+            </div>
+          </div>
+        </section>
+
+        <RightSidebar />
       </main>
     </div>
   );
