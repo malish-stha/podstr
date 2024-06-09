f521f2c8cb65357bff0b036b5d335042c07afd9b
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-06-09 07:44:36 +0545

    "Updated various components and pages with new UI elements and layout changes, added MobileNav component, and configured remote image patterns in next.config.mjs"

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
 import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 
@@ -12,13 +13,18 @@ export default function RootLayout({
       <main className="relative flex bg-black-3">
         <LeftSidebar />
 
-        <section className="border-2 border-red-500 flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+        <section className="border-2 flex min-h-screen flex-1 flex-col px-4 sm:px-14">
           <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
             <div className="flex h-16 items-center justify-between md:hidden">
-              <Image src="/icons/logo.svg" width={30} height={30} alt="img" />
-              MobileNav
+              <Image
+                src="/icons/logo.svg"
+                width={30}
+                height={30}
+                alt="menu icon"
+              />
+              <MobileNav />
             </div>
-            <div>
+            <div className="flex flex-col md:pb-14">
               Toaster
               {children}
             </div>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
+import PodcastCard from "@/components/PodcastCard";
 import { Button } from "@/components/ui/button";
+import { podcastData } from "@/constants";
 
 const Home = () => {
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-        <Button className="text-white-1 bg-orange-1">Button</Button>
+        <div className="podcast_grid">
+          {podcastData.map(({ id, title, description, imgURL }) => (
+            <PodcastCard
+              key={id}
+              imgUrl={imgURL}
+              title={title}
+              description={description}
+              podcastId={id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
